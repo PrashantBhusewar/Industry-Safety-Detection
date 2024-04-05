0add362d6bca5bdd7085d3cca046f08a9ceb302a
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-05 15:14:57 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'PrashantBhusewar',
+    author_email= 'prashantbhusewar1599@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
